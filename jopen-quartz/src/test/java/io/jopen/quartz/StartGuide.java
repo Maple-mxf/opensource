fecfebed5646048a5fef31900402c236f380b91e
ubuntu-m <m17793873123@163.com>
2019-10-22 10:51:36 +0800

    add quartz module

---
+++ jopen-quartz/src/test/java/io/jopen/quartz/StartGuide.java
@@ -0,0 +1,17 @@
+package io.jopen.quartz;
+
+import org.junit.Test;
+
+/**
+ * @author maxuefeng
+ * @since 2019/10/22
+ */
+public class StartGuide {
+
+    @Test
+    public void testSimpleAPI() {
+        // schedule ->
+        // job  jobKey,jobGroup,cron,class  // restart(job)
+
+    }
+}
