a7eb6a582b731e62c21d0d2333ab3162badf565c
ubuntu-m <m17793873123@163.com>
2019-10-23 14:14:41 +0800

    add java memory module

--- jopen-memdb/src/main/java/io/jopen/memdb/query/IntermediateExpression.java
+++ jopen-memdb/src/main/java/io/jopen/memdb/query/IntermediateExpression.java
@@ -1,7 +1,10 @@
 package io.jopen.memdb.query;
 
 /**
- * 中间表达式
+ * 中间表达式  条件构造器  此处的中间表达式暂时定义为断言操作
+ * <p>{@link com.google.common.base.Predicate}</p>
+ * <p>{@link java.util.function.Predicate}</p>
+ *
  * @author maxuefeng
  * @since 2019/10/23
  */
