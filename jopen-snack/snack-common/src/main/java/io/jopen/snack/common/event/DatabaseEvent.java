27e6fc740bb342261701fb4b1efab8a60d27fa1c
ubuntu-m <m17793873123@163.com>
2019-10-28 17:44:16 +0800

    增加事件监听

--- jopen-snack/snack-common/src/main/java/io/jopen/snack/common/event/DatabaseEvent.java
+++ jopen-snack/snack-common/src/main/java/io/jopen/snack/common/event/DatabaseEvent.java
@@ -2,6 +2,8 @@ package io.jopen.snack.common.event;
 
 import io.jopen.snack.common.DatabaseInfo;
 import io.jopen.snack.common.protol.RpcData;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * @author maxuefeng
@@ -13,26 +15,59 @@ public class DatabaseEvent implements SnackApplicationEvent {
 
     private RpcData.C2S.DBOperation dbOperation;
 
-    DatabaseEvent(DatabaseInfo databaseInfo, RpcData.C2S.DBOperation dbOperation) {
+    @Nullable
+    private DatabaseInfo modifyDBInfo;
+
+    DatabaseEvent(@NonNull DatabaseInfo databaseInfo,
+                  RpcData.C2S.DBOperation dbOperation) {
+        this(databaseInfo, dbOperation, null);
+    }
+
+    DatabaseEvent(@NonNull DatabaseInfo databaseInfo,
+                  RpcData.C2S.DBOperation dbOperation,
+                  @Nullable DatabaseInfo modifyDBInfo) {
         this.databaseInfo = databaseInfo;
         this.dbOperation = dbOperation;
+        this.modifyDBInfo = modifyDBInfo;
     }
+
+
+    public DatabaseInfo getDatabaseInfo() {
+        return databaseInfo;
+    }
+
+    public void setDatabaseInfo(DatabaseInfo databaseInfo) {
+        this.databaseInfo = databaseInfo;
+    }
+
+    public RpcData.C2S.DBOperation getDbOperation() {
+        return dbOperation;
+    }
+
+    public void setDbOperation(RpcData.C2S.DBOperation dbOperation) {
+        this.dbOperation = dbOperation;
+    }
+
 
     public static class Create extends DatabaseEvent {
         public Create(DatabaseInfo databaseInfo, RpcData.C2S.DBOperation dbOperation) {
             super(databaseInfo, dbOperation);
         }
+
+        public Create(DatabaseInfo databaseInfo, RpcData.C2S.DBOperation dbOperation, DatabaseInfo targetDatabaseInfo) {
+            super(databaseInfo, dbOperation, targetDatabaseInfo);
+        }
     }
 
     public static class Drop extends DatabaseEvent {
-        public Drop(DatabaseInfo databaseInfo, RpcData.C2S.DBOperation dbOperation) {
-            super(databaseInfo, dbOperation);
+        public Drop(DatabaseInfo databaseInfo, RpcData.C2S.DBOperation dbOperation, DatabaseInfo targetDatabaseInfo) {
+            super(databaseInfo, dbOperation, targetDatabaseInfo);
         }
     }
 
     public static class Modify extends DatabaseEvent {
-        public Modify(DatabaseInfo databaseInfo, RpcData.C2S.DBOperation dbOperation) {
-            super(databaseInfo, dbOperation);
+        public Modify(DatabaseInfo databaseInfo, RpcData.C2S.DBOperation dbOperation, DatabaseInfo targetDatabaseInfo) {
+            super(databaseInfo, dbOperation, targetDatabaseInfo);
         }
     }
 }
--- jopen-snack/snack-common/src/main/java/io/jopen/snack/common/listener/DatabaseListener.java
+++ jopen-snack/snack-common/src/main/java/io/jopen/snack/common/listener/DatabaseListener.java
@@ -1,6 +1,10 @@
 package io.jopen.snack.common.listener;
 
+import io.jopen.snack.common.event.DatabaseEvent;
 import io.jopen.snack.common.event.SnackApplicationEvent;
+import io.jopen.snack.common.exception.ParamIsNullException;
+import io.jopen.snack.common.exception.SnackExceptionUtil;
+import io.jopen.snack.common.protol.RpcData;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
@@ -9,8 +13,34 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  */
 public abstract class DatabaseListener extends SnackApplicationListener {
 
-
     public static class Create extends DatabaseListener {
+        @Override
+        public void handEvent(@NonNull SnackApplicationEvent event) {
+            if (event instanceof DatabaseEvent.Create) {
+                DatabaseEvent createEvent = (DatabaseEvent) event;
+                this.submit(() -> {
+                    RpcData.C2S.DBOperation dbOperation = createEvent.getDbOperation();
+                    SnackExceptionUtil.checkNull(dbOperation, ParamIsNullException.class, "数据库操作符不可为空");
+
+                    switch (createEvent.getDbOperation()) {
+                        case modifyDB: {
+                        }
+                        case createDB: {
+                        }
+                        case showDBs: {
+                        }
+                        case dropDB: {
+                        }
+                        default: {
+                        }
+                    }
+                    return null;
+                }, null);
+            }
+        }
+    }
+
+    public static class Drop extends SnackApplicationListener {
         @Override
         public void handEvent(@NonNull SnackApplicationEvent event) {
 
