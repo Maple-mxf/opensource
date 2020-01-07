460c1b2c95a82eef85c04b463ee9dcaa20f4bd49
ubuntu-m <m17793873123@163.com>
2020-01-07 17:21:42 +0800

    增加快速排序算法Java实现

--- jopen-core/src/main/java/io/jopen/core/algorithm/sort/QuickSort.java
+++ jopen-core/src/main/java/io/jopen/core/algorithm/sort/QuickSort.java
@@ -27,17 +27,15 @@ public class QuickSort {
     }
 
     public static int partition(int[] array, int fromIndex, int toIndex) {
-
         int base = array[fromIndex], i = fromIndex, j = toIndex;
         while (i < j) {
-            while (i < j && array[j] >= base)
-                --j;
-            while (i < j && array[i] <= base)
-                ++i;
-            if (i != j)
-                swap(array, i, j);
+            // 从右向左找一个比base小的
+            while (i < j && array[j] >= base) --j;
+            // 从左向右找一个比base大的
+            while (i < j && array[i] <= base) ++i;
+            if (i != j) swap(array, i, j);
         }
-        swap(array, i, fromIndex);
+        if (i != fromIndex) swap(array, i, fromIndex);
         return i;
     }
 
