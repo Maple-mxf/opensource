08f590ffe821ac380dee4490c307f4a677ff8f08
ubuntu-m <m17793873123@163.com>
2020-01-26 21:34:08 +0800

    增加AQS测试用例

---
+++ jopen-concurrent/src/test/java/io/jopen/util/concurrent/AQSTest.java
@@ -0,0 +1,17 @@
+package io.jopen.util.concurrent;
+
+import org.junit.Test;
+
+/**
+ * {@link java.util.concurrent.locks.AbstractQueuedSynchronizer}
+ *
+ * @author maxuefeng
+ * @since 2020/1/22
+ */
+public class AQSTest {
+
+
+    @Test
+    public void baseApiTest() {
+    }
+}
