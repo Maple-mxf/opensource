0b2d16b6e8fc8e00adef21c42b567e493cbca727
ubuntu-m <m17793873123@163.com>
2020-01-31 11:47:58 +0800

    更改所有依赖的生命周期

---
+++ jopen-springboot-plugins/jopen-springboot-plugin-quartz/src/test/java/io/jopen/springboot/plugin/quartz/test/base/SpringQuartzStarterBaseApi.java
@@ -0,0 +1,17 @@
+package io.jopen.springboot.plugin.quartz.test.base;
+
+import org.junit.Test;
+
+/**
+ * @author maxuefeng
+ * @see org.springframework.scheduling.quartz.QuartzJobBean
+ * @see org.quartz.Scheduler
+ * @since 2020/1/31
+ */
+public class SpringQuartzStarterBaseApi {
+
+    @Test
+    public void baseApiTest1() {
+
+    }
+}
